dd9cdccfebdaa9c90f517f8bd92114182f01fe03
javeria163 <javeria03184057200@gmail.com>
2026-06-13 22:42:49 +0500

    Add solution for: Write a function called fibonacci(n) that returns

---
+++ solution.py
@@ -0,0 +1,23 @@
+# Solution
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# Tests
+
+def test_fibonacci():
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+    assert fibonacci(8) == 21
+    assert fibonacci(9) == 34
