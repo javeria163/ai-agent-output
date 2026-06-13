7f79f8e14e3030e6a149558035836c693df3f9b3
javeria163 <javeria03184057200@gmail.com>
2026-06-13 22:40:22 +0500

    Add solution for: write a function called add(a, b) that returns the

---
+++ solution.py
@@ -0,0 +1,12 @@
+# Solution
+
+def add(a, b):
+    return a + b
+
+# Tests
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    assert add(0, 0) == 0
