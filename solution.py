ea766c16ee340cc01f58b5128c59c57dbd2d4fce
javeria163 <javeria03184057200@gmail.com>
2026-06-13 22:44:21 +0500

    Add solution for: Write a function called bubble_sort(lst) that sort

---
+++ solution.py
@@ -0,0 +1,18 @@
+# Solution
+
+def bubble_sort(lst):
+    n = len(lst)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
+
+# Tests
+
+def test_bubble_sort():
+    assert bubble_sort([64, 34, 25, 12, 22, 11, 90]) == [11, 12, 22, 25, 34, 64, 90]
+    assert bubble_sort([]) == []
+    assert bubble_sort([5]) == [5]
+    assert bubble_sort([5, 1]) == [1, 5]
+    assert bubble_sort([1, 5, 3, 9, 7]) == [1, 3, 5, 7, 9]
