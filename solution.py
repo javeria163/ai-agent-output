bdd4a8ab39b0a5293f92d34304dc1db4eb36623b
javeria163 <javeria03184057200@gmail.com>
2026-06-13 22:43:09 +0500

    Add solution for: Write a function called is_prime(n) that returns T

---
+++ solution.py
@@ -0,0 +1,27 @@
+# Solution
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    max_divisor = int(n**0.5) + 1
+    for d in range(3, max_divisor, 2):
+        if n % d == 0:
+            return False
+    return True
+
+# Tests
+
+def test_is_prime():
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(5) == True
+    assert is_prime(6) == False
+    assert is_prime(23) == True
+    assert is_prime(37) == True
+    assert is_prime(48) == False
+    assert is_prime(59) == True
